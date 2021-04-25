5d9cfbb30758ba6a32fc680d8951c90949eee2e6
developers@twinslash.com <developers@twinslash.com>
2021-04-26 00:10:36 +0300

    remove module test part to another file

--- HW02/Vlad_Bondar/my_enumerable.rb
+++ HW02/Vlad_Bondar/my_enumerable.rb
@@ -25,11 +25,3 @@ module MyEnumerable
     end
   end
 end
-
-using MyEnumerable
-
-ms = [1,2,3,4,5,6,7]
-
-puts ms.my_each { |item| item }
-puts ms.my_map { |item| item+10 }
-puts ms.my_select { |item| item > 4 }
